65a00d35cd558946d088aa97e50ceab6a71e7277
Mateus Rodrigues Baraþal <1900042@salas.aulas>
2019-10-01 07:53:23 -0300

    att

--- ac5 ling/reptil.py
+++ ac5 ling/reptil.py
@@ -1,14 +1,14 @@
 class Reptil:
     def __init__(self, nome, cor_escamas, idade):
         self.nome = nome
-        self.cor_escamas = cor_escamas
+        self.cor_pelo = cor_escamas
         self.idade = idade
 
     def tomar_sol(self):
         return '{} tomando sol'.format(self.nome)
 
     def botar_ovo(self):
-        if self.idade >= 2:
+        if self.idade > 2:
             return '{} botou um ovo'.format(self.nome)
         else:
-            return '{} ainda não atingiu a maturidade sexual'.format(self.nome)
+            return '{} ainda não atingiu maturidade sexual'.format(self.nome)
